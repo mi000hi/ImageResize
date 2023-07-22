c8c2af769168674941c38913cb0913f5e514c638
mi000hi <michaelroth@hispeed.ch>
2023-07-22 12:56:17 +0200

    initial sourcefiles commit

---
+++ main.py
@@ -0,0 +1,257 @@
+from PIL import Image,  ImageOps
+
+import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.filedialog as fd
+from PIL import ImageTk, Image, ImageDraw
+
+import glob
+import os
+
+Image.MAX_IMAGE_PIXELS = 933120000
+
+aspect_ratio_original = 4.0/3.0
+aspect_ratio_new = 16.0/9.0
+# aspect_ratio_new = 21.0/9.0
+
+window_width = 1100
+window_height = 900
+window_size = f"{window_width}x{window_height}"
+
+image_height = 700
+image_width = int(image_height*aspect_ratio_original)
+
+
+
+class TKWindow():
+
+
+    root = None
+    button_open = None
+    button_save = None
+    button_skip = None
+    image_directory_path = None
+    image_paths = None
+    image_index = None
+    panel_image = None
+    current_image_path = None
+    current_tk_image_raw = None
+    current_tk_image_drawn = None
+    image_resized = None
+    canvas = None
+    canvas_rectangle = None
+    rectangle = None
+    input_quality_entry = None
+
+    image_output_quality = 85
+
+
+    def save_image(self, image):
+
+        quality = int(self.input_quality_entry.get())
+        # print(f"quality: {quality}")
+
+        exif = self.img_raw.info['exif']
+
+        img_filename = os.path.basename(self.current_image_path)
+        image.save(f"{self.image_directory_path_output}/{img_filename}", quality=quality, optimize=True, exif=exif)
+
+    def update_tk_image(self, tk_image):
+        # if self.panel_image is not None:
+        #     self.panel_image.destroy()
+
+        # self.canvas_image = tk.Label(self.root, image = tk_image)
+        # self.panel_image.pack(side = "left", fill = "both", expand = "yes")
+
+        self.canvas_image = self.canvas.create_image(0,0, anchor=tk.NW, image=tk_image)
+        # self.panel_image.pack(side = "left", fill = "both", expand = "yes")
+
+    def load_new_image(self):
+        self.image_index = self.image_index%len(self.image_paths)
+
+        # print(self.progress['value'])
+        self.progress['value'] = int(self.image_index/len(self.image_paths)*100)
+        self.progress.update_idletasks()
+
+        self.current_image_path = self.image_paths[self.image_index]
+        print(f"new image: {self.current_image_path}")
+        self.img_raw = Image.open(self.current_image_path)
+        self.img_raw = ImageOps.exif_transpose(self.img_raw)
+        # img_resized = img_raw.rotate(-90)
+
+        ratio = self.img_raw.width/self.img_raw.height
+
+        width = int(image_height*ratio)
+        height = image_height
+
+        if width > window_width-100:
+            width = window_width-100
+            height = int(width/ratio)
+        self.img_resized = self.img_raw.resize((width, height))
+
+
+        self.current_tk_image_raw = ImageTk.PhotoImage(self.img_resized)
+
+    def button_skip_function(self):
+        print("skipping image")
+
+        self.image_index += 1
+        self.load_new_image()
+        self.update_tk_image(self.current_tk_image_raw)
+
+    def button_save_and_skip_function(self):
+        print("saving and skipping image")
+        self.save_image(self.img_raw)
+
+        self.image_index += 1
+        self.load_new_image()
+        self.update_tk_image(self.current_tk_image_raw)
+
+    def button_crop_and_save_function(self):
+        print("cropping and saving image")
+
+        img = self.img_raw.copy()
+        # print(f"shape: {img.size}, img={img}")
+
+        ratio = img.width/image_width
+
+
+        y1 = int(self.rectangle[0][1]*ratio)
+        y2 = int(y1+img.width/aspect_ratio_new)
+        if y2 > img.height:
+            y2 = int(image_height*ratio)
+            y1 = int(y2-img.width/aspect_ratio_new)
+        crop_points = (0, y1, img.width, y2)
+        img = img.crop(crop_points)
+        # img = img.resize(new_dimension, Image.LANCZOS)
+
+        self.save_image(img)
+
+        self.button_skip_function()
+        
+
+    def button_open_function(self):
+
+        print("selecting folder")
+        
+        self.image_directory_path = fd.askdirectory()
+        print(f"selected directory: {self.image_directory_path}")
+
+        # get image path list
+        self.image_paths = glob.glob(f"{self.image_directory_path}/*.jpg")
+        self.image_index = 250
+
+        self.load_new_image()
+        self.update_tk_image(self.current_tk_image_raw)
+
+        self.image_directory_path_output = fd.askdirectory()
+        print(f"selected output directory: {self.image_directory_path_output}")
+
+    def button_convert_all_except_16_9_function(self):
+        while self.image_index > 0:
+            if self.img_raw.width/self.img_raw.height != 16.0/9.0:
+                self.button_save_and_skip_function()
+            else:
+                self.button_skip_function()
+
+
+    def button_last_function(self):
+        print("returning to last image")
+
+        self.image_index -= 1
+        self.load_new_image()
+        self.update_tk_image(self.current_tk_image_raw)
+
+    def button_convert_all_until_change_function(self):
+        
+        current_width = self.img_raw.width
+        current_height = self.img_raw.height
+
+        while self.img_raw.width == current_width and self.img_raw.height == current_height:
+            self.button_save_and_skip_function()
+
+
+    def key_handler(self, event):
+        print(event.char, event.keysym, event.keycode)
+
+        if event.keysym == 'Return':
+            self.button_crop_and_save_function()
+        elif event.keysym == 'Shift_R':
+            self.button_save_and_skip_function()
+        elif event.keysym == 'space':
+            self.button_skip_function()
+        elif event.keysym == 'BackSpace':
+            self.button_last_function()
+
+
+    def click_handler(self, event):
+
+        # print(event.x, event.y)
+        # print(f"winfo_x: {self.canvas.winfo_x()}, winfo_y: {self.canvas.winfo_y()}")
+
+        mouse_x = event.x
+        mouse_y = event.y
+        canvas_x = self.canvas.winfo_x()
+        canvas_y = self.canvas.winfo_y()
+
+        self.rectangle = ((0+2,mouse_y), (image_width-1,mouse_y+int(image_width/aspect_ratio_new)))
+
+        # draw rect on image
+        self.canvas.delete(self.canvas_rectangle)
+        self.canvas_rectangle = self.canvas.create_rectangle(self.rectangle, outline="red")
+
+    def __init__(self):
+        #This creates the main window of an application
+        self.root = tk.Tk()
+        self.root.title("Image resizer")
+        self.root.geometry(window_size)
+        self.root.configure(background='grey')
+
+        self.root.bind("<Key>", self.key_handler)
+
+        # add buttons
+        self.button_open = tk.Button(self.root, text = "open directory", command = self.button_open_function)
+        self.button_skip = tk.Button(self.root, text = "skip", command = self.button_skip_function)
+        self.button_save_and_skip = tk.Button(self.root, text = "save and skip", command = self.button_save_and_skip_function)
+        self.button_save = tk.Button(self.root, text = "crop and save", command = self.button_crop_and_save_function)
+        self.button_last = tk.Button(self.root, text = "last image", command = self.button_last_function)
+        self.button_convert_all_until_change = tk.Button(self.root, text = "convert until res change", command = self.button_convert_all_until_change_function)
+        self.button_convert_all_except_16_9 = tk.Button(self.root, text = 'convert except 16/9', command=self.button_convert_all_except_16_9_function)
+
+        #The Pack geometry manager packs widgets in rows or columns.
+        self.button_open.pack(side="top")
+        self.button_skip.pack(side="top")
+        self.button_save_and_skip.pack(side="top")
+        self.button_save.pack(side="top")
+        self.button_last.pack(side="top")
+        self.button_convert_all_until_change.pack(side="top")
+        self.button_convert_all_except_16_9.pack(side="top")
+
+        self.input_quality_entry = tk.Entry(self.root)
+        self.input_quality_entry.insert(0, str(self.image_output_quality))
+        self.input_quality_entry.pack(side="top")
+
+        self.canvas = tk.Canvas(self.root)
+        self.canvas.bind("<B1-Motion>", self.click_handler)
+        self.canvas.pack(side="bottom", fill=tk.BOTH, expand=1)
+
+        # add progress bar
+        self.progress = ttk.Progressbar(self.root, orient='horizontal', mode='determinate', length=1000)
+        self.progress.pack(side='bottom')
+
+        #Start the GUI
+        self.root.mainloop()
+
+    
+
+image_directory_path = None
+image_paths = None
+image_index = None
+panel = None
+window = None
+img = None
+
+window = TKWindow()
+
+
+
